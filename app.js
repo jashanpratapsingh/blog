ae2a78ea08e91d69f588f2c2aa405002caca2e3e
Jashan Pratap Singh <jashanpratap123@gmail.com>
2022-01-20 00:55:45 +0530

    made changes to the backend to record the composed messages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const res = require("express/lib/response");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,8 +16,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res) {
-    res.render("home", { startingContent: homeStartingContent })
+    const content = {
+        startingContent: homeStartingContent,
+        posts: posts
+    }
+    res.render("home", content);
 });
 
 app.get("/about", function(req, res) {
@@ -31,13 +38,16 @@ app.get("/compose", function(req, res) {
     res.render("compose")
 });
 
+
 app.post("/compose", function(req, res) {
-    console.log(req.body.postTitle);
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.Text1
+    };
+    posts.push(post);
+
+    res.redirect("/")
 });
-
-
-
-
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
