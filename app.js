74cb9dce120774152aed121fb81e0ed486b1c34e
Jashan Pratap Singh <jashanpratap123@gmail.com>
2022-01-19 22:33:16 +0530

    Added the home and about page

--- app.js
+++ app.js
@@ -17,11 +17,15 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     res.render("home", { startingContent: homeStartingContent })
-})
+});
 
+app.get("/about", function(req, res) {
+    res.render("about", { about: aboutContent })
+});
 
-
-
+app.get("/contact", function(req, res) {
+    res.render("contact", { contact: contactContent })
+});
 
 
 
